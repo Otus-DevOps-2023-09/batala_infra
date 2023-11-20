b5dab95f480c94315797469985b0f3d56378a5a0
apbutyukov <>
2023-11-20 18:15:22 +0300

    terraform-2 -1

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update
+echo Waiting for apt-get to finish...
+a=1; while [ -n "$(pgrep apt-get)" ]; do echo $a; sleep 1s; a=$(expr $a + 1); done
 sudo apt install mongodb -y
 sudo systemctl start mongodb
 sudo systemctl enable mongodb
