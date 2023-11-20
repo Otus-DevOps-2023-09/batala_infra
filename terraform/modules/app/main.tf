747a551081cd5d942a47617a6e302f642b094f7e
apbutyukov <>
2023-11-20 21:11:13 +0300

    terraform-2 -c v cont700

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -11,14 +11,7 @@ provider "yandex" {
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
-#
-  image_id                 = var.image_id
-  bucket_name              = var.backet_name
-  service_account_id       = var.service_account_id
-  access_key               = var.access_key
-  secret_key               = var.secret_key
-  db_id                    = var.db_id
-  private_key_path         = var.private_key_path 
+
 }
 
 resource "yandex_compute_instance" "db" {
