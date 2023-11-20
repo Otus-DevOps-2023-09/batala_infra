8b54b17d40ae20c9dcf015a27f270938d0c85d48
apbutyukov <>
2023-11-20 18:51:13 +0300

    terraform-2 -4

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -4,29 +4,52 @@ variable "cloud_id" {
 variable "folder_id" {
   description = "Folder"
 }
+variable "zone" {
+  description = "Zone"
+  default     = "ru-central1-a"
+}
 variable "public_key_path" {
   description = "Path to the public key used for ssh access"
 }
 variable "private_key_path" {
-  description = "Connection private key file"
+  description = "Path to the private key used for ssh access"
+}
+variable "image_id" {
+  description = "Disk image"
+}
+variable "subnet_id" {
+  description = "Subnet"
 }
 variable "service_account_key_file" {
   description = "key .json"
 }
-variable "zone" {
-  description = "Zone"
-  # Значение по умолчанию
-  default = "ru-central1-a"
+variable "service_account_id" {
+  description = "SA id"
+}
+variable "instances_count" {
+  description = "Default count of instances"
+  type        = number
+  default     = 1
 }
 variable "app_disk_image" {
   description = "Disk image for reddit app"
-  default = "reddit-app-base"
+  default     = "reddit-app-base"
 }
-variable "subnet_id" {
-  description = "Subnets for modules"
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
-variable "db_ip_address" {
-  description = "Database IP-address"
+variable "bucket_name" {
+  description = "Name of S3 bucket"
+}
+variable "db_id" {
+  description = "YC dynamodb Database ID"
+}
+variable "access_key" {
+  description = "S3 access key"
+}
+variable "secret_key" {
+  description = "S3 secret key"
 }
 variable "env" {
   description = "Environment stage, prod"
