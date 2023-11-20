3574d885cda88282b8a8997b020fb12b363d4ef1
apbutyukov <>
2023-11-20 19:48:04 +0300

    terraform-2 -tests validate cont

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -16,7 +16,7 @@ module "app" {
   folder_id                = var.folder_id
   zone                     = var.zone
   env                      = var.env
-#
+  #
   bucket_name              = var.bucket_name
   secret_key               = var.secret_key
   service_account_id       = var.service_account_id
@@ -41,4 +41,15 @@ module "db" {
   folder_id                = var.folder_id
   zone                     = var.zone
   env                      = var.env
+  bucket_name              = var.bucket_name
+  secret_key               = var.secret_key
+  service_account_id       = var.service_account_id
+  image_id                 = var.image_id
+  bucket_name              = var.backet_name
+  service_account_id       = var.service_account_id
+  service_account_key_file = var.service_account_key_file
+  access_key               = var.access_key
+  secret_key               = var.secret_key
+  db_id                    = var.db_id
+  private_key_path         = var.private_key_path  
 }
